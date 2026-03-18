017db6e42e1ccedc14613b3a68d01d400f55e460
Ishita <ishitaizzy@gmail.com>
2026-03-18 13:46:40 +0530

    fix eslint error

--- src/App.js
+++ src/App.js
@@ -163,7 +163,7 @@ function useInView(threshold = 0.15) {
     const obs = new IntersectionObserver(([e]) => { if (e.isIntersecting) setVisible(true); }, { threshold });
     if (ref.current) obs.observe(ref.current);
     return () => obs.disconnect();
-  }, []);
+  }, [threshold]);
   return [ref, visible];
 }
 
